625b22f0268cea9476fbc08ba2d6cdb68aa5148a
nlimpid <jshuangzl@gmail.com>
2021-02-13 23:45:39 +0800

    get: add content-length

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ use std::{
 
 extern crate clap;
 use clap::{App, Arg, ArgMatches, SubCommand};
+use indicatif::{ProgressBar, ProgressStyle};
 use log::logger;
 mod proxy;
+use lust::get_file_size;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let matches = App::new("xcgit")
@@ -27,6 +31,20 @@ fn main() {
     run(matches);
 }
 
+fn getProgress(f: String, total_size: u64) {
+    let fs = get_file_size(&f);
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+.progress_chars("#>-"));
+
+    while fs < total_size {
+        let fs = get_file_size(&f);
+        pb.set_position(fs);
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
+
 fn run(matches: ArgMatches) -> Result<(), String> {
     // ...
     let logger = log::logger();
@@ -38,8 +56,13 @@ fn run(matches: ArgMatches) -> Result<(), String> {
 }
 
 fn download(url: String) -> Result<(), Box<dyn Error>> {
-    let result = reqwest::blocking::get(&url)?.bytes()?;
     let path = Path::new("lorem_ipsum.tar.gz");
+
+    let resp = reqwest::blocking::get(&url)?;
+    let file_size = resp.content_length().unwrap();
+
+    thread::spawn(move || getProgress("lorem_ipsum.tar.gz".to_string(), file_size));
+    let result = resp.bytes()?;
 
     let mut file = File::create(&path)?;
 
